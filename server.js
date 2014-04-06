9882cf6aed6346137e7e657b78d26a36609cd68e
nowaygit <675308992@qq.com>
2014-04-06 15:37:14 +0800

    V0.1 Initial commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.configure(function () {
     app.set('port', process.env.PORT || conf.port);
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'ejs');
-    app.use(express.favicon());
+//    app.use(express.favicon());
     //app.use(express.logger('dev'));
     app.use(express.json());
     app.use(express.urlencoded());
